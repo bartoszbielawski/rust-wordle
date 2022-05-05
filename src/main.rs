7d5feec3a70e124324821f3f86d08b761426bbff
Bartosz Bielawski <bartosz.bielawski@gmail.com>
2022-05-05 10:05:08 +0200

    WIP: OO refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use termion::color;
 use std::io;
 use std::fs;
 use std::collections::HashMap;
-
+use colored::Colorize;
 
 #[derive(PartialEq,Debug)]
 enum LetterState
@@ -16,12 +16,95 @@ enum LetterState
 	RightPlace
 }
 
-const color_map: HashMap<char, termion::color::Color> = [
-	(LetterState::Unknown, color::LightWhite),
-	(LetterState::NotPresent, color::White),
-	(LetterState::WrongPlace, color::LightYellow),
-	(LetterState::RightPlace, color::LightGreen),
-];
+enum GuessResult
+{
+	WrongInput,
+	Try(GuessContent),
+	Won(GuessContent),
+	Lost(String),
+}
+
+struct Game
+{
+	word_list: Vec<String>,
+	letter_states: HashMap<char, LetterState>,
+	hidden_word: String,
+	letter_counter: Counter::<char>,
+	try_no: u32,
+	finished: bool,
+}
+
+struct GuessContent
+{
+	try_no: u32,
+	word_letter_states: Vec<LetterState>,
+}
+
+
+impl Game
+{
+	fn new(words: Vec<String>) -> Self
+	{
+		let mut game = Game {word_list: words, 
+				letter_states: HashMap::<char, LetterState>::new(), 
+				hidden_word: "".to_string(), 
+				letter_counter: Counter::<char>::new(),
+				try_no: 0, 
+				finished: false};
+			
+		let mut rng = rand::thread_rng();
+		
+		game.hidden_word = game.word_list.choose(&mut rng).unwrap().to_string();  
+		game.letter_counter.update(game.hidden_word.chars());
+		
+		return game;
+	}
+	
+	fn word_valid(&self, word: &String) -> bool
+	{
+		if word.len() != 5
+		{
+			return false;
+		}
+		
+		if !self.word_list.contains(&word)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	fn guess_word(&mut self, guess: &String) -> GuessResult
+	{
+		if !self.word_valid(guess)
+		{
+			return GuessResult::WrongInput;
+		}
+			
+		let mut letter_counter = self.letter_counter.clone();
+		
+		let mut wls = vec![(); 5]; 
+		
+		for i in 0..=4
+		{
+			println!("{}", i);
+		}
+		
+		for (ch1, ch2) in zip(guess.chars(), self.hidden_word.chars())
+		{
+			let chars_equal = ch1 == ch2;
+			if chars_equal
+			{
+				
+			}
+		}
+		
+		
+		GuessResult::Lost(self.hidden_word)
+	}
+}
+
 
 fn guess_word(hidden: &str, guess: &str, letter_states: &mut HashMap::<char, LetterState>) -> bool
 {
@@ -29,28 +112,37 @@ fn guess_word(hidden: &str, guess: &str, letter_states: &mut HashMap::<char, Let
 	hidden_counter.update(hidden.chars());
 	//println!("{:?}", hidden_counter);
 	
-	let mut all_found = true;
-	
 	if hidden.len() != guess.len()
 	{
-		println!("{} {}", hidden.len(), guess.len());
 		return false;
 	}
+	
+	if guess == hidden
+	{
+		return true;
+	}
+	
+	//a variable for state of all letters in the word
+	//HERE
 	
 	for (ch1, ch2) in zip(guess.chars(), hidden.chars())
 	{
 		let chars_equal = ch1 == ch2;
-		all_found = all_found & chars_equal;
 		
-		let letter_state = letter_states.entry(ch1).or_insert(LetterState::NotPresent);
-		
-		if chars_equal
+		//all letters were already created
+		let letter_state = letter_states.get_mut(&ch1).unwrap();
+		if !chars_equal
 		{
-			print!("{}{}", color::Fg(color::LightGreen), ch1);
-			hidden_counter.subtract(ch1.to_string().chars());		//remove one
-			*letter_state = LetterState::RightPlace;
-			continue;  
+			continue;
 		}
+		
+		print!("{}{}", color::Fg(color::LightGreen), ch1);
+		hidden_counter.subtract(ch1.to_string().chars());		//remove one
+		*letter_state = LetterState::RightPlace;
+	}
+	
+	for (ch1, ch2) in zip(guess.chars(), hidden.chars())
+	{
 		let letter_present = hidden_counter.contains_key(&ch1);
 		if letter_present
 		{
@@ -65,7 +157,10 @@ fn guess_word(hidden: &str, guess: &str, letter_states: &mut HashMap::<char, Let
 			continue;
 		}
 		
-		*letter_state = LetterState::NotPresent;
+		if (*letter_state == LetterState::WrongPlace) || (*letter_state == LetterState::RightPlace)
+		{
+			*letter_state = LetterState::NotPresent;
+		}
 		print!("{}{}", color::Fg(color::Reset), ch1);
 	}
 	println!("{}", color::Fg(color::Reset));
@@ -80,7 +175,7 @@ fn load_words(filename: &str) -> Option<Vec<String>>
 	let words: Vec<String> = data.lines().
 		filter(|x| x.len() == 5).
 		filter(|x| x.chars().all(char::is_alphabetic)).
-		map(|x|    x.to_string()).
+		map(|x|    x.to_lowercase()).
 		filter(|x| x.is_ascii()).
 		collect();
 	
@@ -142,11 +237,20 @@ fn main()
 			break;
 		}
 		
+		print!("\t\t");
 		for ch in 'a'..'z'
 		{
-			
-			print!("{} - {:?}", ch, letter_state.get(&ch).unwrap());
+			let ls = letter_state.get(&ch).unwrap();
+			let color: colored::Color = match ls
+			{
+				LetterState::Unknown => colored::Color::White,
+				LetterState::NotPresent => colored::Color::Black,
+				LetterState::RightPlace => colored::Color::BrightGreen,
+				LetterState::WrongPlace => colored::Color::BrightYellow,
+			}; 
+			print!("{}", ch.to_string().color(color));
 		}
+		println!();
 	}
 	
 	if !guessed
